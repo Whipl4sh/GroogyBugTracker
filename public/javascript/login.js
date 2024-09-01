6300bb1e465bae236325d4d87ea95512ddac3018
Roberto Espeso <roberto.espeso96@gmail.com>
2024-09-01 16:56:18 -0300

    Formularios registro e ingreso
    Creación de HTML - CSS y JS para los popups de Registro e inicio de sesión.
    
    Con validación de formulario y campos requeridos
    
    Reseteo de campos al clickear el botón cancelar.
    
    Mediaqueries para distintos tamaños de viewport.
    
    Uso de variables css para mantener standar de propiedades, colores, tamaños de fuentes, etc.

---
+++ public/javascript/login.js
@@ -0,0 +1,19 @@
+const popupLogin = document.getElementById('popup-login');
+const popupRegister = document.getElementById('popup-register');
+const btnLogin = document.getElementById('btn-login');
+const btnRegister = document.getElementById('btn-register');
+const btnCloseDialog = document.querySelectorAll('.btn-close-dialog');
+const btnContainers = document.querySelectorAll('.buttons-container')
+var tamañoNavegador = window.innerWidth
+
+
+btnLogin.addEventListener('click', () =>{
+    popupLogin.showModal();
+})
+btnRegister.addEventListener('click',() => {
+    popupRegister.showModal();
+})
+btnCloseDialog.forEach((btn) => btn.addEventListener('click', () =>{
+    popupLogin.close();
+    popupRegister.close();
+}))
