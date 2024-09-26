345343260a7493290e387faef283c19a82aafbd8
Norma Suriano <surii2908@gmail.con>
2024-09-26 00:49:52 -0300

    reparando enlaces

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", 
   
   function() {
-    fetch('../../views/partials/header/header.html')
+    fetch('../views/partials/header/header.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header-container').innerHTML = data;
